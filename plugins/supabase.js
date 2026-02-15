25aaf27396472ea11578a48014c6a4c40aceab86
Kananat <kananat.c4@gmail.com>
2026-02-15 18:09:51 +0700

    move supabase to env

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -1,18 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
 import fetch, { Headers, Request, Response } from 'cross-fetch'
 
-// --- ไม้ตาย: บังคับประกาศตัวแปร Global ให้ Server รู้จัก ---
+// fix SSR
 if (!global.Headers) {
   global.Headers = Headers
   global.Request = Request
   global.Response = Response
   global.fetch = fetch
 }
-// -----------------------------------------------------
 
-// ใส่ URL และ Key ของคุณตามเดิม
-const supabaseUrl = 'https://grrcubjdzdyifdmkrmlv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdycmN1YmpkemR5aWZkbWtybWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNDg2MTMsImV4cCI6MjA4NDkyNDYxM30.MgjLEybnli23MeuUno0A3iNK-dIGqrzP89ekJOKLXcc'
+// 🔥 ใช้ ENV แทน hardcode
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
